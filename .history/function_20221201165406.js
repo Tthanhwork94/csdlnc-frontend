0272008992eefe22e8e3b5581776046198184ed1
tanthanh <dtthanh.work@gmail.com>
2022-12-01 21:43:39 +0700

    thêm chức năng khách hàng xử lý giỏ hàng

---
+++ .history/function_20221201165406.js
@@ -0,0 +1,49 @@
+function setAccountLocal(username){
+    $.ajax({
+        url: "http://localhost:8080/api/taikhoan/"+username,
+        type: 'GET',
+        dataType: 'json',
+        success: function(data){
+            window.localStorage.setItem('account', JSON.stringify(data));
+            console.log(JSON.stringify(data));
+        },
+        error: function(data){
+            alert("Không tìm thấy tài khoản.");
+        }
+    });
+}
+
+function logout(){
+    window.localStorage.removeItem("username");
+    window.localStorage.removeItem("account");
+    window.localStorage.removeItem("cart");
+    // setTimeout(function() {
+    //     window.location.href = "/index.html";
+    //    }, 100);
+}
+
+function init(){
+    document.getElementById("account").innerHTML = JSON.parse(localStorage.getItem("account")).tendangnhap;
+    let cart= '{"listmon":[],"sdtkhachhang":"","diachigiaohang":"","tuychon":""}';
+    var object = JSON.parse(cart);
+    window.localStorage.setItem("cart",JSON.stringify(object));
+}
+
+
+// function getMenu(madoitac){
+//     var obj;
+//     $.ajax({
+//         url: "http://localhost:8080/api/doitac/menu/"+madoitac,
+//         type: 'GET',
+//         dataType: 'json',
+//         success: function(data){
+//             // console.log(JSON.parse(JSON.stringify(data)));
+//             obj = JSON.parse(JSON.stringify(data));
+//             obj.
+//         },
+//         error: function(data){
+//             alert("Không tìm thấy menu.");
+//         }
+//     });
+// }
+
