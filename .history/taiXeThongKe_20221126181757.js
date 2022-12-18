b6b9c719f54c68b21bd4d73ce66a08a14b6214fe
tanthanh <dtthanh.work@gmail.com>
2022-12-18 12:09:07 +0700

    CSDLNC - chỉnh sửa thông tin đơn hàng

---
+++ .history/taiXeThongKe_20221126181757.js
@@ -0,0 +1,35 @@
+// Check nhập
+$(function() {
+    $("form[name='thongKeThang']").validate({
+      rules: {
+        doanhthuthang: "required"
+      },
+      messages: {
+        doanhthuthang: "Hãy nhập tháng"
+      },
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
+  });
+// xem doanh thu ngày
+var order = document.getElementById("xemdoanhthungay");
+order.onclick = function () {
+  var check =document.getElementById("doanhthungay").value ;
+  if (check!=""){
+    var str ="Số đơn hàng: 30 | Doanh thu: 600000vnd"
+    document.getElementById("table1").innerHTML = str;
+    }
+  }
+
+// xem doanh thu tháng
+var revenueMonth = document.getElementById("xemdoanhthuthang");
+revenueMonth.onclick = function () {
+  var check =document.getElementById("doanhthuthang").value ;
+  if (check!=""){
+    var str ="Số đơn hàng: 900 | Doanh thu: 18000000vnd"
+    document.getElementById("table2").innerHTML = str;
+    }
+  }
+
+
